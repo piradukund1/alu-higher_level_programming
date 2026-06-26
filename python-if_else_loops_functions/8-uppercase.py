0de86f352228ba8130e2534dde92a64d16427efe
piradukund1 <p.iradukund1@alustudent.com>
2026-06-26 13:18:49 +0200

    damn this is crazy bro

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str):
+    result = ""
+    for c in str:
+        if ord('a') <= ord(c) <= ord('z'):
+            result += "{}".format(chr(ord(c) - 32))
+        else:
+            result += "{}".format(c)
+    print("{}".format(result))
